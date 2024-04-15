bc3cb72c851a56ffc8d6bca3a82699d092bd9ae6
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-04-15 16:23:06 +0200

    initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunction, MetaFunction } from "@remix-run/node";
+import {
+  Form,
+  json,
+  useActionData,
+  useFormAction,
+  useNavigation,
+} from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +14,36 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+
+  console.log(body);
+
+  return json({ action_url: `test` });
+};
+
 export default function Index() {
+  const action = useFormAction();
+  const data = useActionData();
+  const { state } = useNavigation();
+
+  console.log(state, data);
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Podcast generator</h1>
+
+      <Form action={action} method="POST">
+        <label htmlFor="url">URL</label>
+        <input
+          type="text"
+          name="url"
+          id="url"
+          disabled={state === "submitting"}
+        />
+
+        <button>Submit</button>
+      </Form>
     </div>
   );
 }
