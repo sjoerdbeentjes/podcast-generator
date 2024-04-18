58605f2afc24d1c21e57f39925b559e5132f3d6e
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-04-19 00:12:13 +0200

    update UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,17 @@
 import type { ActionFunction, MetaFunction } from "@remix-run/node";
 import {
-  Form,
+  Form as RemixForm,
   json,
   useActionData,
   useFormAction,
   useNavigation,
 } from "@remix-run/react";
 import { Octokit } from "octokit";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { BlobReader, BlobWriter, ZipReader } from "@zip.js/zip.js";
+import { Button } from "../components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Card } from "~/components/ui/card";
 
 type Workflow = Awaited<
   ReturnType<Octokit["rest"]["actions"]["getWorkflow"]>
@@ -68,14 +72,42 @@ export default function Index() {
   const action = useFormAction();
   const data = useActionData<Workflow>();
   const { state } = useNavigation();
+  const formRef = useRef<HTMLFormElement>(null);
   const [loading, setLoading] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
 
-  async function downloadArtifact(id: number) {
+  async function getArtifect(id: number) {
     const response = await fetch(`/get-artifact?run_id=${id}`);
     const artifact = (await response.json()) as Artifact;
 
     if (artifact.url) {
-      window.location.href = artifact.url;
+      setDownloadUrl(artifact.url);
+
+      formRef.current?.reset();
+    }
+  }
+
+  async function downloadArtifactFromZip(url: string) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const zipReader = new ZipReader(new BlobReader(blob));
+
+    const entries = await zipReader.getEntries();
+
+    for (const entry of entries) {
+      if (entry.filename.endsWith(".mp3")) {
+        if (!entry.getData) {
+          console.error("Entry does not have a getData method");
+          return;
+        }
+        const mp3Blob = await entry.getData(new BlobWriter());
+        const mp3Url = URL.createObjectURL(mp3Blob);
+
+        const a = document.createElement("a");
+        a.href = mp3Url;
+        a.download = "podcast.mp3";
+        a.click();
+      }
     }
   }
 
@@ -97,29 +129,70 @@ export default function Index() {
 
           console.log("Run completed!");
 
-          downloadArtifact(data.id);
+          getArtifect(data.id);
         }
       }, 1000);
     }
   }, [data]);
 
   return (
-    <div>
-      <h1>Podcast generator</h1>
+    <div className="flex flex-col h-screen justify-center">
+      <Card className="w-full max-w-lg m-auto">
+        <header className="p-6">
+          <h1 className="text-2xl font-semibold leading-none tracking-tight">
+            Podcast Generator
+          </h1>
+        </header>
 
-      <Form action={action} method="POST">
-        <label htmlFor="url">URL</label>
-        <input
-          type="text"
-          name="url"
-          id="url"
-          disabled={loading || state === "submitting"}
-        />
+        <RemixForm
+          ref={formRef}
+          action={action}
+          method="POST"
+          className="p-6 pt-0 grid w-full items-center gap-4"
+        >
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="url"
+            >
+              URL
+            </label>
 
-        <button disabled={loading || state === "submitting"}>Submit</button>
+            <Input
+              type="text"
+              name="url"
+              id="url"
+              disabled={loading || state === "submitting"}
+            />
+          </div>
 
-        {loading && <p>Creating podcast...</p>}
-      </Form>
+          <Button
+            disabled={loading || state === "submitting"}
+            className="justify-self-start"
+          >
+            Submit
+          </Button>
+        </RemixForm>
+
+        {(downloadUrl || loading) && (
+          <div className="flex flex-col gap-4 p-6 border-t items-start">
+            {loading && (
+              <p>Creating podcast... (this may take a few minutes)</p>
+            )}
+
+            {downloadUrl && <p>Your podcast has been created! </p>}
+
+            {downloadUrl && (
+              <Button
+                variant="secondary"
+                onClick={() => downloadArtifactFromZip(downloadUrl)}
+              >
+                Download mp3
+              </Button>
+            )}
+          </div>
+        )}
+      </Card>
     </div>
   );
 }
